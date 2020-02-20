59c6cc730c3498f9d600ef43b746fb82e81ae56b
Sunny <volokonskiy52@gmail.com>
2020-02-20 22:04:48 +0200

    added product

--- autoload.php
+++ autoload.php
@@ -2,9 +2,10 @@
 
 spl_autoload_register(function ($class)
 {
-    $fileName = $class . 'php';
+    $fileName = $class . '.php';
     if (!file_exists($fileName)) {
         return false;
     }
+
     include $fileName;
 });
--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
 
-require_once 'autoload.php';
+include 'autoload.php';
 
-if (isset($_REQUEST['product']) && isset($_REQUEST['price'])) {
-    $newProduct     = new Product($_REQUEST['product'], $_REQUEST['price']);
+    $newProduct     = new Product('car', 10);
+    $newProduct     = new Product('banana', 2);
+    $newProduct     = new Product('mouse', 5);
+    $newProduct     = new Product('bread', 3);
+    $newProduct     = new Product('cat', 12);
     $basket         = new Basket();
     $basket->addProduct($newProduct);
     $getSumProducts = $basket->calculatePrice($basket->getBasketWithProducts());
-}
+
+    var_dump($getSumProducts);
