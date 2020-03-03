18f155548be21bcf07c82ba712d3d0fd83dd4b64
Sunny <volokonskiy52@gmail.com>
2020-03-04 00:23:32 +0200

    update code style

--- index.php
+++ index.php
@@ -16,12 +16,15 @@ $products[] = new Product('mouse', 5);
 $products[] = new Product('bread', 3);
 $products[] = new Product('cat', 12);
 $products[] = new Product('banana', 20);
+$products[] = new Product('keyboard', 30);
 
 foreach ($products as $product) {
     $basket->addProduct($product);
 }
-$discount   = new StandardDiscount('car', 3, 50, $basket);
-
+$discountCar    = new StandardDiscount('car', 3, 50, $basket);
+$discountBanana = new StandardDiscount('banana', 3, 50, $basket);
+$discountCar->applyDiscounts();
+$discountBanana->applyDiscounts();
 $basketSum = $basket->calculatePrice();
 
 var_dump($basketSum);
