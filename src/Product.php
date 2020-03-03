f595bd4dd3ac76e8f19bc2c2271e0c62dfe4d711
avolokonskyi <alexander.volokonskyi@sigma.software>
2020-03-03 11:11:49 +0200

    update code 1.9

--- src/Product.php
+++ src/Product.php
@@ -8,11 +8,10 @@ class Product
     public  $discountable = false;
     public  $productCount = 1;
 
-    public function __construct($productType, $price, Basket $basket)
+    public function __construct($productType, $price)
     {
         $this->productType = $productType ;
         $this->price       = $price;
-        $basket->addProduct($this);
     }
 
     public function setDiscountability($discountable)
