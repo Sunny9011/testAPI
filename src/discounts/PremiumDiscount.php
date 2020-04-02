d71cbaab74161e73f7ef41dbf015d470a2ca6955
Sunny <volokonskiy52@gmail.com>
2020-04-02 09:59:41 +0300

    added second discoutn

---
+++ src/discounts/PremiumDiscount.php
@@ -0,0 +1,21 @@
+<?php
+
+
+class PremiumDiscount extends BaseDiscount
+{
+    public function getProductType()
+    {
+        return $this->productType;
+    }
+
+    public function getProductCount()
+    {
+        return $this->productCount;
+    }
+
+    public function getDiscount()
+    {
+        return $this->discount;
+    }
+
+}
