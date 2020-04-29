f1c98505637c05ff691791dd314e82f99d95f510
avolokonskyi <alexander.volokonskyi@sigma.software>
2020-04-29 13:14:01 +0300

    update tests

--- unitTest/BasketTest.php
+++ unitTest/BasketTest.php
@@ -48,10 +48,51 @@ class BasketTest extends TestCase
      */
     public function testAddProduct()
     {
-        $currentProduct = array('car', 3, 50);
+        $currentProduct = array('car', 10);
+
         $this->basket->addProduct($this->product);
         $this->assertEquals($this->basket, $currentProduct, 'The products do not meet the expected result.');
     }
 
+    /**
+     * Price with discount for basket
+     */
+    public function testGetPriceWithDiscount()
+    {
+        $currentPrice = ['car', 15];
 
+        $products = [
+            0 => ['car', 10],
+            1 => ['car', 10],
+            2 => ['car', 10],
+            ];
+        $discount = 50;
+        $newPrice = $this->basket->shouldReceive('getPriceWithDiscount')->with($products, $discount);
+
+        $this->assertEquals($newPrice, $currentPrice, 'Prices do not match result.');
+    }
+
+    /**
+     * Discount for basket
+     */
+    public function testGetDiscountProductsFromBasket()
+    {
+        $currentBasket = [0 =>
+            ['productType'  =>  'car'],
+            ['price'        =>  10],
+            ['discountable' =>  true],
+            ['productCount' =>  1]
+        ];
+
+        $basket = [0 =>
+            ['productType'  =>  'car'],
+            ['price'        =>  10],
+            ['discountable' =>  false],
+            ['productCount' =>  1]
+        ];
+        $this->basket->shouldReceive('getDiscount');
+        $basketWithDiscount = $this->basket->getDiscountProductsFromBasket($basket);
+
+        $this->assertEquals($basketWithDiscount, $currentBasket,'Discount not set for basket.');
+    }
 }
